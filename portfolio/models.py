cac12f9d362458a0e9879c1572ae551442adf886
victor98 <victorrzna@gmail.com>
2023-05-16 13:14:08 +0200

    first commit

---
+++ portfolio/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    enlace = models.URLField(verbose_name="Enlace", null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+
+    class Meta:
+        verbose_name = "Proyecto"
+        verbose_name_plural = "Proyectos"
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return self.title
