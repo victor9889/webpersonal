dbf4aaa93f2d560d63c4e244406a4e6486140e25
victor98 <victorrzna@gmail.com>
2023-05-16 13:32:28 +0200

    cambios

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class PortfolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portfolio'
+    name = 'portfoli0'
     verbose_name = 'Portafolio'
